780770d37d5cbaa8ca4a86c510a4c6919e69a4d3
auscarter17 <98193003+auscarter17@users.noreply.github.com>
2022-02-22 23:13:38 -0500

    added arrays to respond to confirmations

--- Develop/script.js
+++ Develop/script.js
@@ -3,34 +3,60 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-// var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-// var numeric = ["0123456789"];
-// var symbols = ["!@#$%^&*()-_=+{};:,<.>/?`~"];
+// vars for types of data based on user input
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var numeric = ["0123456789"];
+var symbols = ["!@#$%^&*()-_=+{};:,<.>/?`~"];
 
 
 
 function generatePassword() {
+  var password = "";
+  var inputChar = "";
+  // Ask user for number of characters in their password
   var charLength = parseInt(prompt("How many characters should your password be? Pick a number between 8-128.")); 
     if (charLength < 8) {
       window.alert("Your password is too short. Pick a number between 8-128.");
-      return null;
+      return (generatePassword());
 
     }else if (charLength > 128) {
       window.alert("Your password is too long. Pick a number between 8-128.");
-      return null;
+      return (generatePassword());
       
     }else if (Number.isNaN(charLength)) {
-      window.alert("Please enter a number between 8-128 and no other characters.")
+      window.alert("Please enter a number between 8-128 and no other characters.");
+      return (generatePassword());
     }
   
-  var upperCase = window.confirm("Should your password include uppercase letters?");
+  // Ask user for confirmation of types of characters contained in password
+  var inputUpper = window.confirm("Should your password include uppercase letters?");
+    if (inputUpper) {
+      inputChar += inputLower;
+    }
 
-  var lowerCase = window.confirm("Should your password contain lowercase letters?");
+  var inputLower = window.confirm("Should your password contain lowercase letters?");
+    if (inputLower) {
+      inputChar += inputLower;
+    }
 
-  var numberic = window.confirm("Should your password contain numbers?");
+  var inputNumeric = window.confirm("Should your password contain numbers?");
+    if (inputNumeric) {
+      inputChar += inputNumeric;
+    }
 
-  var symbols = window.confirm("Should your password contain special characters?");
+  var inputSymbols = window.confirm("Should your password contain special characters?");
+    if (inputSymbols) {
+      inputChar += inputSymbols;
+    }
+
+  if (!inputUpper 
+  && !inputLower
+  && !inputNumeric
+  && !inputSybols) {
+    window.alert("You must choose at least one type of selector.");
+    return (generatePassword());
+  }
 
   
 }
@@ -38,9 +64,10 @@ function generatePassword() {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  
 
   var passwordText = document.querySelector("#password");
-
+  passwordText.length = charLength;
   passwordText.value = password;
   
 
